47f1a0fa693dba43f03ece0aad19d35990c87cff
Andrew Wong <andrew12345@live.com>
2014-07-03 21:55:08 +1000

    * There's a zero-width character. I can't find it. Give up and use alternative ... x.length - 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,55 @@
-    var hasargs;
-    var parameters = window.location.toString();
-    if (parameters.indexOf("?") + 1 != 0) { //If there are parameters
-      hasargs = true;
-      parameters = parameters.substring(parameters.indexOf("?") + 1).split("&");
-      var img = new Image();
-      img.onload = function () {}
-      img.onerror = function () {
-        console.log('Page not found! Redirecting...');
-        document.location.href = '404.html';
-      }
-      img.src = '../assets/products/' + parameters[0] + '/product.png';
-
-      var xmlhttp;
-      var productinfo;
-      if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-      } else { // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-          productinfo = xmlhttp.responseText.split('\n');
-          // productinfo[0] -> Name
-          // productinfo[1] -> Price
-          // productinfo[2] -> Stock
-          // productinfo[3] -> Description
-        }
-      }
-      xmlhttp.open("GET", "../assets/products/" + parameters[0] + "/info.txt", true);
-      xmlhttp.send();
-    } else {
-      // Show product listing maybe?
-      hasargs = false;
+function beginload() {
+  if (parameters.indexOf("?") + 1 != 0) { //If there are parameters
+    hasargs = true;
+    parameters = parameters.substring(parameters.indexOf("?") + 1).split("&");
+    var img = new Image();
+    img.onerror = function () {
+      console.log('Page not found! Redirecting...');
+      document.location.href = '404.html';
     }
+    img.src = '../assets/products/' + parameters[0] + '/product.png';
+  } else {
+    hasargs = false;
+  }
+}
 
-    function finishload() {
-      if (window.hasargs) {
-        document.getElementById('pcontent').style.display = "inherit";
-        document.getElementById('product_location').innerHTML = "<a href='site.html'>Home</a> > <a href='products'>Products</a> > <a href='"+productinfo[0].substring(productinfo[0].indexOf(';') + 1).toLowerCase()+"'>" + productinfo[0].substring(productinfo[0].indexOf(';') + 1) + "</a> > <b>" + productinfo[0].substring(0, productinfo[0].indexOf(';')) + "</b>";
-        document.getElementById('productname').innerHTML = productinfo[0].substring(0, productinfo[0].indexOf(';'));
-        if (productinfo[1].contains(';')) {
-          //Discount
-          priceinfo = productinfo[1].split(';');
-          document.getElementById('productdatabaseinfo_price').innerHTML = "<strike>$" + priceinfo[0] + "</strike> $" + priceinfo[2] + "<br>" + priceinfo[1] + "% discount";
-        } else {
-          //No discount
-          document.getElementById('productdatabaseinfo_price').innerHTML = productinfo[1];
-        }
-        document.getElementById('productdatabaseinfo_stock').innerHTML = productinfo[2];
-        document.getElementById('productdescription').innerHTML = productinfo[3];
-      }
-      //Set product category
-      document.getElementById("product_category_" + productinfo[0].substring(productinfo[0].indexOf(';') + 1).toLowerCase()).className += ' active';
-      document.getElementById('productimage').src = "../assets/products/" + parameters[0] + "/product.png";
+function loadproduct(productcode) {
+  var xmlhttp;
+  if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  } else { // code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      productinfo = xmlhttp.responseText.split('\n');
+      // productinfo[0] -> Name
+      // productinfo[1] -> Price
+      // productinfo[2] -> Stock
+      // productinfo[3] -> Description
     }
+  }
+  xmlhttp.open("GET", "../assets/products/" + productcode + "/info.txt", false);
+  xmlhttp.send();
+}
+
+function replace() {
+  productcategory = productinfo[0].substring(productinfo[0].indexOf(';') + 1).toLowerCase();
+  productname = productinfo[0].substring(0, productinfo[0].indexOf(';'));
+  document.getElementById('pcontent').style.display = "inherit";
+  document.getElementById('product_location').innerHTML = "<a href='site.html'>Home</a> > <a href='products'>Products</a> > <a href='" + productcategory + "'>" + productinfo[0].substring(productinfo[0].indexOf(';') + 1) + "</a> > <b>" + productname + "</b>";
+  document.getElementById('productname').innerHTML = productname
+  if (productinfo[1].indexOf(";") > -1) {
+    //Discount
+    priceinfo = productinfo[1].split(';');
+    document.getElementById('productdatabaseinfo_price').innerHTML = "<strike>$" + priceinfo[0] + "</strike> $" + priceinfo[2] + "<br>" + priceinfo[1] + "% discount";
+  } else {
+    //No discount
+    document.getElementById('productdatabaseinfo_price').innerHTML = "$" + productinfo[1];
+  }
+  document.getElementById('productdatabaseinfo_stock').innerHTML = productinfo[2];
+  document.getElementById('productdescription').innerHTML = productinfo[3];
+  //Set product category
+  document.getElementById('product_category_' + productcategory.substring(0, (productcategory.length - 1))).className += ' active';; //Can't find the zero-width space
+  document.getElementById('productimage').src = "../assets/products/" + parameters[0] + "/product.png";
+}
