8e7f03663ead1cffab49a486eca78ac6a932b5a1
Andrew Wong <andrew12345@live.com>
2014-07-08 23:02:26 +1000

    * products -> products.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,7 @@ function replace() {
   if (productvalid) {
     productcategory = productinfo[0].substring(productinfo[0].indexOf(';') + 1).toLowerCase().trim(); //Seems to be a zero-width character somewhere, although trimming fixes the issue.
     productname = productinfo[0].substring(0, productinfo[0].indexOf(';'));
-    document.getElementById('product_location').innerHTML = "<a href='home.html'><img style='margin-right: 2px; padding-bottom: 4px; vertical-align: middle;' src='../assets/images/home.png' alt='Home logo'/>Home</a> > <a href='products'>Products</a> > <a href='" + productcategory + "'>" + productinfo[0].substring(productinfo[0].indexOf(';') + 1) + "</a> > <b>" + productname + "</b>";
+    document.getElementById('product_location').innerHTML = "<a href='home.html'><img style='margin-right: 2px; padding-bottom: 4px; vertical-align: middle;' src='../assets/images/home.png' alt='Home logo'/>Home</a> > <a href='products.html'>Products</a> > <a href='" + productcategory + "'>" + productinfo[0].substring(productinfo[0].indexOf(';') + 1) + "</a> > <b>" + productname + "</b>";
     document.getElementById('productname').innerHTML = productname
     if (productinfo[1].indexOf(";") > -1) {
       //Discount
