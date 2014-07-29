c0d14a52a399c164d54bba474a83f070df6cbbc5
Andrew Wong <andrew12345@live.com>
2014-07-29 18:09:49 +1000

    * Move product.js into here

--- assets/js/page_products.js
+++ assets/js/page_products.js
@@ -37,6 +37,59 @@ function loadfourohfour() {
   xmlhttp.send();
 }
 */
+function loadProduct(productcode) {
+	loadPath("products/" + productcode + "/info.txt", loadProduct_cont);
+}
+
+function loadProduct_cont() {
+		productready = true;
+		try {
+			productinfo = result.contentWindow.document.body.innerHTML.replace(/(<([^>]+)>)/ig, "").split("\n");
+		} catch (e) {
+	/* Redundant as the index wrapper blocks chrome (Local file issue only)
+    // Cross domain error, most likely
+    //
+    // Just chrome???
+    // - "SecurityError: Blocked a frame with origin "null" from accessing a cross-origin frame."
+	*/
+			productready = false;
+		}
+		// productinfo[0] -> Name
+		// productinfo[1] -> Price
+		// productinfo[2] -> Availability
+		// productinfo[3] -> Description
+		document.head.removeChild(result);
+	}
+	/* Kind of stupid, but I don't care - Andrew
+	// It would work better if I used a web server, but the assignment is submitted locally. Eh
+	// And GitHub doesn't support custom 404 pages.
+	// And I can't be bothered putting this on my vps.
+	*/
+
+	/*
+	function loadproduct(productcode) {
+	   // Webserver only
+	  var xmlhttp;
+	  if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	    xmlhttp = new XMLHttpRequest();
+	  } else { // code for IE6, IE5
+	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	  xmlhttp.onreadystatechange = function () {
+	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	      productready = true;
+	      productinfo = xmlhttp.responseText.split('\n');
+	      // productinfo[0] -> Name
+	      // productinfo[1] -> Price
+	      // productinfo[2] -> Availability
+	      // productinfo[3] -> Description
+	    }
+	  }
+	  xmlhttp.open("GET", "../assets/products/" + productcode + "/info.txt", true);
+	  //xmlhttp.open("GET", "products/" + productcode + "/info.txt", true);
+	  xmlhttp.send();
+	}
+	*/
 
 function finishLoad() {
 	document.getElementById('container').style.display = 'inherit'; // aka showDocument() @ util.js;
